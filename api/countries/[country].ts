c954833df1c1c504c182730dbdc5617ea3171d22
Pavel Klochkov <paul.klochkov@gmail.com>
2020-04-05 20:46:35 +0200

    Small refactor

--- api/countries/[country].ts
+++ api/countries/[country].ts
@@ -7,17 +7,19 @@ const countryPage = async (req: any, res: any) => {
 
   if (req.query && req.query.country) {
     const dataByCountry = await getDataByCountry(req.query.country);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
 
     if (dataByCountry.dates && dataByCountry.dates[0]) {
-      res.setHeader("Access-Control-Allow-Origin", '*');
-      res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-      res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
       return res.json(dataByCountry);
     }
     res.status(500);
     return res.json(error);
   }
-
   res.status(500);
   return res.json(error);
 };
