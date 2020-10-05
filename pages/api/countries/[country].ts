b33bc57e9c21a74953ef350d245353c5d1f1ff0e
Pavel Klochkov <paul.klochkov@gmail.com>
2020-10-06 00:38:12 +0200

    Add country ids to API

--- pages/api/countries/[country].ts
+++ pages/api/countries/[country].ts
@@ -1,28 +1,39 @@
 import { getDataByCountry } from '../../../helpers';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { getCountriesList } from '../../../helpers/getCountriesList';
 
-const countryPage = async (req: NextApiRequest, res: NextApiResponse) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
+const countryPage = async (req: NextApiRequest, response: NextApiResponse) => {
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
   );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  response.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   const error = {
     error: { message: `No data found for ${req.query.country}` },
   };
 
-  if (req.query && req.query.country) {
-    const dataByCountry = await getDataByCountry(req.query.country);
+  if (req?.query?.country && !Array.isArray(req?.query?.country)) {
+    let country = '';
+    const countryId = Number.parseInt(req?.query?.country);
+
+    if (countryId || countryId === 0) {
+      const allCountries: string[] = await getCountriesList();
+      country = allCountries[countryId];
+    } else {
+      country = req.query.country;
+    }
+
+    const dataByCountry = await getDataByCountry(country);
 
     if (dataByCountry.dates && dataByCountry.dates[0]) {
-      return res.json(dataByCountry);
+      return response.json(dataByCountry);
     }
-    res.status(500);
-    return res.json(error);
+    response.status(500);
+    return response.json(error);
   }
-  res.status(500);
-  return res.json(error);
+  response.status(500);
+  return response.json(error);
 };
 
 export default countryPage;
